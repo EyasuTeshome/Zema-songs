77e56d1d744133a121937c7d1c486c23617bfe58
Josh <joshtesh91@mail.com>
2022-06-30 23:44:39 +0800

    included html css and javascript files for the capstone project

--- js-file/index.js
+++ js-file/index.js
@@ -5,55 +5,61 @@ const listItem = document.querySelector('.menu-list');
 const speakersList = document.querySelector('.performers1')
 
 hamburger.addEventListener('click', () => {
-    mobMenu.classList.toggle('active');
+  mobMenu.classList.toggle('active');
 });
 
 closeBtn.addEventListener('click', () => {
-    mobMenu.classList.remove('active');
+  mobMenu.classList.remove('active');
 });
 
 listItem.addEventListener('click', () => {
-    mobMenu.classList.remove('active');
-    console.log("clicked")
+  mobMenu.classList.remove('active');
+  console.log('clicked');
 });
 
-
 const guests = [{
-        name: 'Abenezer Desta',
-        discription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, quia.',
-        title: 'Singer',
-        image: './img/abeni.jfif'
-    },
-    {
-        name: 'Birhanu Tekle',
-        discription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, quia.',
-        title: 'Singer',
-        image: './img/birhanu.jpg'
+  name: 'Abenezer Desta',
+  discription: 'Lorem ipsum dolor sit elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, quia.',
+  title: 'Singer',
+  image: './img/abeni.jfif',
+},
+{
+  name: 'Birhanu Tekle',
+  discription: 'Lorem ipsum dolor sit elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, quia.',
+  title: 'Singer',
+  image: './img/birhanu.jpg',
 
-    },
-    {
-        name: 'Hanan Mahidi',
-        discription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, quia.',
-        title: 'singer',
-        image: './img/hanan.jfif'
-    },
-    {
-        name: 'Israel',
-        title: 'Musician',
-        discription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, quia.',
-        image: './img/israel.jfif'
-    },
-    {
-        name: 'Exodus',
-        title: 'Musician',
-        discription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, quia.',
-        image: './img/exo.png'
+},
+{
+  name: 'Hanan Mahidi',
+  discription: 'Lorem ipsum dolor sit elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, quia.',
+  title: 'singer',
+  image: './img/hanan.jfif',
+},
+{
+  name: 'Israel',
+  title: 'Musician',
+  discription: 'Lorem ipsum dolor sit elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, quia.',
+  image: './img/israel.jfif',
+},
+{
+  name: 'Exodus',
+  title: 'Musician',
+  discription: 'Lorem ipsum dolor sit elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, quia.',
+  image: './img/exo.png',
 
-    }
-]
+},
+{
+  name: 'Exodus',
+  title: 'Musician',
+  discription: 'Lorem ipsum dolor sit elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, quia.',
+  image: './img/exo.png',
+
+},
+];
 
 document.addEventListener('DOMContentLoaded', () => {
-    speakersList.innerHTML = guests.map((guest) => `
+  speakersList.innerHTML = guests.map((guest) => `
  <div class="performers-2">
             
             <div class="performer1">
@@ -68,5 +74,5 @@ document.addEventListener('DOMContentLoaded', () => {
                 </div>
             </div>
         </div>
- `)
+        `).join('');
 });
