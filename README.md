5c72c9a9b5ecbc94a81377b7eb1c7d250ff1e245
Josh <joshtesh91@mail.com>
2022-07-01 18:33:31 +0800

    fix comments given by the code reviewer

--- README.md
+++ README.md
@@ -1,17 +1,13 @@
 
 
-# Personal Portifolio
+# Zema Songs Concert
 
 
-This website is a website for the youth of Apostolic Church. In this website the youth can check upcoming confrences and confrence places.
+This website is a website is for Zema Songs Concert. In this website the 2022 Concert is presented including the address, the performers and the programs arrangment.
 
-In this project I have used linter for HTML and CSS, to make sure there are no linter errors on the code. This portifolio website contains four sections:
-1. header
-2. home
-3. Work
-4. about-me
-5. Contact-me
-
+In this project I have used linter for HTML and CSS, to make sure there are no linter errors on the code. This portifolio website contains 2 pages:
+1. Home page
+2. About page
 ## Built With
 
 - HTML
@@ -23,7 +19,8 @@ In this project I have used linter for HTML and CSS, to make sure there are no l
 
 (https://eyasuteshome.github.io/Portfolio.github.io/)
 
-
+## Acknowledgment
+This Project design is taken from Cindy Shin[https://www.behance.net/adagio07]. If you are using this design in the future please make sure to give credit to Cindy Shin.
 ## Getting Started
 
 To successfully utilize this project first of all you need to clone or download the repository. Next, open the index.html file to check the website’s main page. If you want to add or edit the files you can do that by simply opening the file folder in VS code.
--- js-file/index.js
+++ js-file/index.js
@@ -2,7 +2,7 @@ const hamburger = document.querySelector('.hamburger');
 const mobMenu = document.querySelector('.mobile-menu');
 const closeBtn = document.querySelector('.close-button');
 const listItem = document.querySelector('.menu-list');
-const speakersList = document.querySelector('.performers1')
+const speakersList = document.querySelector('.performers1');
 
 hamburger.addEventListener('click', () => {
   mobMenu.classList.toggle('active');
@@ -14,7 +14,6 @@ closeBtn.addEventListener('click', () => {
 
 listItem.addEventListener('click', () => {
   mobMenu.classList.remove('active');
-  console.log('clicked');
 });
 
 const guests = [{
@@ -22,47 +21,50 @@ const guests = [{
   discription: 'Lorem ipsum dolor sit elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, quia.',
   title: 'Singer',
   image: './img/abeni.jfif',
+  class: 'visible',
 },
 {
   name: 'Birhanu Tekle',
   discription: 'Lorem ipsum dolor sit elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, quia.',
   title: 'Singer',
   image: './img/birhanu.jpg',
-
+  class: 'visible',
 },
 {
   name: 'Hanan Mahidi',
   discription: 'Lorem ipsum dolor sit elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, quia.',
   title: 'singer',
   image: './img/hanan.jfif',
+  class: 'hidden1',
 },
 {
   name: 'Israel',
   title: 'Musician',
   discription: 'Lorem ipsum dolor sit elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, quia.',
   image: './img/israel.jfif',
+  class: 'hidden2',
 },
 {
   name: 'Exodus',
   title: 'Musician',
   discription: 'Lorem ipsum dolor sit elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, quia.',
   image: './img/exo.png',
-
+  class: 'hidden3',
 },
 {
   name: 'Exodus',
   title: 'Musician',
   discription: 'Lorem ipsum dolor sit elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, quia.',
   image: './img/exo.png',
-
+  class: 'hidden4',
 },
 ];
 
 document.addEventListener('DOMContentLoaded', () => {
   speakersList.innerHTML = guests.map((guest) => `
- <div class="performers-2">
+ <div class="performers-2 ${guest.class}">
             
-            <div class="performer1">
+            <div id="" class="performer1">
                 <div class="img-container">
                     <img class="performer-img" src="${guest.image}" alt="">
                 </div>
